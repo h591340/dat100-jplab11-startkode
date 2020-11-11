6533d57265fa93478cce97e5aa3b54ed13748a86
Muaz <moaz-@192.168.0.154>
2020-11-11 21:17:33 +0100

    Det er en feil på oppgave 3

--- src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
+++ src/no/hvl/dat100/jplab11/oppgave1/Innlegg.java
@@ -3,6 +3,12 @@ package no.hvl.dat100.jplab11.oppgave1;
 import no.hvl.dat100.jplab11.common.TODO;
 
 public abstract class Innlegg {
+	protected int id;
+	protected String bruker;
+	protected String dato;
+	protected int likes;
+	
+	
 	
 	// TODO - deklarering av objektvariable
 	
@@ -11,61 +17,83 @@ public abstract class Innlegg {
 	}
 	
 	public Innlegg(int id, String bruker, String dato) {
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=0;
 
 		// TODO 
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		
 	}
 
 	public Innlegg(int id, String bruker, String dato, int likes) {
 
 		// TODO - START
+		this.id=id;
+		this.bruker=bruker;
+		this.dato=dato;
+		this.likes=likes;
 		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		//throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
 	}
 	
 	public String getBruker() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		return this.bruker;
 
 	}
 
 	public void setBruker(String bruker) {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		this.bruker=bruker;
 	}
 
 	public String getDato() {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		return this.dato;
 		
 	}
 
 	public void setDato(String dato) {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		this.dato=dato;
 	}
 
 	public int getId() {
-		throw new UnsupportedOperationException(TODO.method());
-
+		//throw new UnsupportedOperationException(TODO.method());
+		return this.id;
 	}
 
 	public int getLikes() {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		return this.likes;
 
 	}
 	
 	public void doLike () {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		this.likes=++this.likes;
 	}
 	
 	public boolean erLik(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		if(this.getId()==innlegg.id)
+		{
+			return true;
+		}
+		else
+			return false;
 
 	}
 	
 	@Override
 	public String toString() {
 		
-		throw new UnsupportedOperationException(TODO.method());
-				
+		//throw new UnsupporteSdOperationException(TODO.method());
+		String resultat;
+		resultat=this.id+"\n"+this.bruker+"\n"+this.dato+"\n"+this.likes+"\n";
+		return resultat;
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
--- src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
+++ src/no/hvl/dat100/jplab11/oppgave3/Blogg.java
@@ -6,46 +6,91 @@ import no.hvl.dat100.jplab11.oppgave1.*;
 public class Blogg {
 
 	// TODO: objektvariable 
+	private Innlegg [] innleggtabell;
+	private int nesteledig;
 
 	public Blogg() {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		//throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell=new Innlegg[20];
 	}
 
 	public Blogg(int lengde) {
-		throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		//throw new UnsupportedOperationException(TODO.constructor("Blogg"));
+		innleggtabell=new Innlegg[lengde];
 	}
 
 	public int getAntall() {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		return this.nesteledig;
 	}
 	
 	public Innlegg[] getSamling() {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		return this.innleggtabell;
 
 	}
 	
 	public int finnInnlegg(Innlegg innlegg) {
-
-		throw new UnsupportedOperationException(TODO.method());
+		int found=0;
+		for(int i=0;i<innleggtabell.length;i++)
+		{
+			if(innleggtabell[i].erLik(innlegg))
+			{
+				found=i;
+			}
+			
+		}
+		return found;
+		
+		//throw new UnsupportedOperationException(TODO.method());
+		
 	}
 
 	public boolean finnes(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		if(innlegg.getId()==nesteledig)
+		{
+			return true;
+		}
+		else 
+			return false;
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public boolean ledigPlass() {
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		if(nesteledig<innleggtabell.length-1)
+		{
+			return true;
+		}
+		else
+			return false;
 
 	}
 	
 	public boolean leggTil(Innlegg innlegg) {
 
-		throw new UnsupportedOperationException(TODO.method());
+		//throw new UnsupportedOperationException(TODO.method());
+		if(ledigPlass()==true)
+		{
+			innleggtabell[nesteledig]=innlegg;
+			nesteledig++;
+			return true;
+		}
+		return false;
+		
 	}
 	
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+		//throw new UnsupportedOperationException(TODO.method());
+		String resultat;
+		resultat=""+nesteledig+"\n";
+		for(int i=0;i<nesteledig;i++)
+		{
+			resultat+=innleggtabell[i].toString();
+		}
+		return resultat;
+		}
+	
 
 	// valgfrie oppgaver nedenfor
 	
